de508a6b13d30d5d95155c8cbca1f4795465d013
Daria Abushenko <2009add@gmail.com>
2022-07-10 17:48:55 +0500

    added ESLint and Prettier

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,7 @@
-export const switchTheme = (theme: string, setTheme: any) => {
+export const switchTheme = (
+  theme: string,
+  setTheme: React.Dispatch<React.SetStateAction<string | undefined>>
+) => {
   const newTheme = theme === 'light' ? 'dark' : 'light';
   setTheme(newTheme);
 };
